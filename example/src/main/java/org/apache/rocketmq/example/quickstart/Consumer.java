63b343d5b4f92be96d53918bc453e516c78f2b7c
beneu-ubuntu <beneulwq@163.com>
2022-11-30 20:47:39 +0800

    修改生产者 消费者 配置

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -37,39 +37,13 @@ public class Consumer {
          * Instantiate with specified consumer group name.
          */
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
-
-        /*
-         * Specify name server addresses.
-         * <p/>
-         *
-         * Alternatively, you may specify name server addresses via exporting environmental variable: NAMESRV_ADDR
-         * <pre>
-         * {@code
-         * consumer.setNamesrvAddr("name-server1-ip:9876;name-server2-ip:9876");
-         * }
-         * </pre>
-         */
-        // Uncomment the following line while debugging, namesrvAddr should be set to your local address
-//        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
-
-        /*
-         * Specify where to start in case the specific consumer group is a brand-new one.
-         */
+        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-
-        /*
-         * Subscribe one more topic to consume.
-         */
         consumer.subscribe(TOPIC, "*");
-
-        /*
-         *  Register callback to execute on arrival of messages fetched from brokers.
-         */
         consumer.registerMessageListener((MessageListenerConcurrently) (msg, context) -> {
             System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
-
         /*
          *  Launch the consumer instance.
          */
